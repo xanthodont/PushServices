d7dcded47e249d4938b109025cba6475d950822f
xanthodont <xanthodont@hotmail.com>
2015-04-04 17:09:09 +0800

    mqtt服务器端实现代码上传

---
+++ mqtt-broker/src/main/java/org/dna/mqtt/moquette/messaging/spi/impl/events/StopEvent.java
@@ -0,0 +1,10 @@
+package org.dna.mqtt.moquette.messaging.spi.impl.events;
+
+/**
+ * Poison pill to shutdown the event loop
+ *
+ * @author andrea
+ */
+public class StopEvent extends MessagingEvent {
+    
+}
